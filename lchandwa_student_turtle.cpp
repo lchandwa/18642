8e2ba2d1d15440963ed2a282cc1a0bb381c64f37
lchandwa <lchandwa@andrew.cmu.edu>
2019-09-07 02:20:20 -0400

    version5.0

--- lchandwa_student_turtle.cpp
+++ lchandwa_student_turtle.cpp
@@ -4,12 +4,14 @@
  *
  * STUDENT NAME: Lavina Chandwani
  * ANDREW ID: lchandwa
- * LAST UPDATE: 1:20 AM 
+ * LAST UPDATE: 2:10 AM 
  *
  * This file was originally an algorithm to solve the ece642rtle maze
  * using the left-hand rule. The code was intentionaly left obfuscated.
  * 
- * The code has been modified to use the right-hand rule. T
+ * The code has been modified to use the right-hand rule. The code has
+ * been cleaned up to maintain consistency and make it easy to understand.
+ *
  */
 
 #include "student.h"
@@ -19,7 +21,7 @@ turtleMove studentTurtleStep(bool bumped) {return MOVE;}
 
 // OK TO MODIFY BELOW THIS LINE
 
-#define TIMEOUT 40    // bigger number slows down simulation so you can see what's happening
+const float TIMEOUT = 40;    // bigger number slows down simulation so you can see what's happening
 float w, cs;
 float fx1, fy1, fx2, fy2;
 float z, aend, mod, bp, q;
@@ -51,62 +53,73 @@ bool studentMoveTurtle(QPointF& pos_, int& nw_or) {
        
 	bp = bumped(fx1,fy1,fx2,fy2);
 	aend = atend(pos_.x(), pos_.y());
-
+        
+        //Turtle orientation conditions
+        //Turtle moves right at each step (very first step is in forward direction)
+        //If turtle faces bump it will keep turning to its left until it finds an
+        //open path
         if(nw_or == LEFT) {
-	    if(cs == 2) {
-                nw_or = 1;  
-                cs = 1; 
+	    if(cs == move_trle) {
+                nw_or = UP;  
+                cs = bump_trle; 
             }
 	    else if (bp) {
-                nw_or = 3;  
-                cs = 0; 
+                nw_or = DOWN;  
+                cs = stop_trle; 
             }
 	    else {
-                cs = 2;
+                cs = move_trle;
             }
         }
 	else if(nw_or == UP) {
-	    if(cs == 2) { 
-                nw_or = 2; 
-                cs = 1; 
+	    if(cs == move_trle) { 
+                nw_or = RIGHT; 
+                cs = bump_trle; 
             }
 	    else if (bp) {
-                nw_or = 0;  
-                cs = 0;
+                nw_or = LEFT;  
+                cs = stop_trle;
             }
 	    else {
-                cs = 2;
+                cs = move_trle;
             }
         }
 	else if(nw_or == RIGHT) {
-	    if(cs == 2) { 
-                nw_or = 3;
-                cs = 1;
+	    if(cs == move_trle) { 
+                nw_or = DOWN;
+                cs = bump_trle;
             }
 	    else if (bp) {
-                nw_or = 1;
-                cs = 0;
+                nw_or = UP;
+                cs = stop_trle;
             }
 	    else {
-                cs = 2;
+                cs = move_trle;
             }
 	}
 	else if(nw_or == DOWN) {
-	    if(cs == 2) { 
-                nw_or = 0;
-                cs = 1; 
+	    if(cs == move_trle) { 
+                nw_or = LEFT;
+                cs = bump_trle; 
             }
 	    else if (bp) { 
-                nw_or = 2;
-                cs = 0;
+                nw_or = RIGHT;
+                cs = stop_trle;
             }
 	    else {
-                cs = 2;
+                cs = move_trle;
             }
         }
+
 	ROS_INFO("Orientation=%f  STATE=%f", nw_or, cs);
-        z = (cs == 2);
+
+        //whenever cs is equal to move_trle turtle moves 1 step forward
+        z = (cs == move_trle);
+
         mod = true;
+        
+        //turtle movement condition, movement depends on the direction 
+        //turtle is facing
 	if(z == true && aend == false) {
             switch(nw_or) {
                 case UP: pos_.setY(pos_.y() - 1);
@@ -123,12 +136,18 @@ bool studentMoveTurtle(QPointF& pos_, int& nw_or) {
             mod = true;
         }
     }
+    
+    //at final goal turtle stops moving
     if (aend) {
         return false;
     }
+
+    //turtle orientation available after timeout
     (w==0)? w  = TIMEOUT: w -= 1;
+    
     if (w==TIMEOUT) {
         return true;
     }
+
     return false;
 }
